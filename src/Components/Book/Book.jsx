74b93af204beef6526975155cf055d311bb02b3e
Khaiyum-Ahmed <sayedshakin666@gmail.com>
2025-08-09 16:48:30 +0600

    Book card successfully shown

---
+++ src/Components/Book/Book.jsx
@@ -0,0 +1,37 @@
+import { FaRegStar } from "react-icons/fa";
+
+const Book = ({ book }) => {
+    const { author, bookName, category, image, rating, tags } = book;
+    return (
+        <div className='border border-[rgba(255,255,255,.2)] rounded-2xl p-6'>
+            <div className="card w-full">
+                <figure className='bg-[#F3F3F3] rounded-2xl py-8 mb-6'>
+                    <img
+                        className='max-w-36 max-h-[186px] skew-6 rotate-6 rounded-md'
+                        src={image}
+                        alt={bookName} />
+                </figure>
+
+                <div className="card-actions justify-start mb-4">
+                    {tags.map((tag, idx) =><div key={idx} className=" bg-[rgba(35,190,10,.05)] rounded-4xl py-2 px-5 text-base font-medium text-[#23BE0A]">{tag}</div>)}
+                       
+                </div>
+
+                <div className='border-b pb-6 border-dashed border-[rgba(255,255,255,.15)]'>
+                    <h2 className=" font-bold text-2xl mb-4">
+                        {bookName}
+                    </h2>
+                    <p className='font-medium text-[rgba(255,255,255,.8)]'>By : {author}</p>
+                    
+                </div>
+                <div className='mt-5 flex justify-between items-center'>
+                    <p className='font-medium text-[rgba(255,255,255,.8)]'>{category}</p>
+                    <p className="flex justify-center items-center gap-2 font-medium text-xl text-[rgba(255,255,255,.8)] ">{rating} <span><FaRegStar /></span></p>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Book from '../Book/Book';
+
+const Books = () => {
+    const [books, setBooks] = useState([]);
+    useEffect(() => {
+        fetch('../../../public/booksData.json')
+            .then(res => res.json())
+            .then(data => setBooks(data))
+            
+    }, [])
+    console.log(books)
+    return (
+        <div>
+            <h1 className='font-bold text-5xl text-center my-9'>Books</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-32'>
+                {
+                    books.map(book => <Book key={book.bookId} book = {book}></Book>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Books;
